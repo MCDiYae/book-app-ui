b5f3dd0ef83283a4fe0bc4999309c07754bddb4f
MCDiYae <meddiyae.ghbalou@gmail.com>
2023-05-31 23:19:54 +0100

    structure

--- lib/presentation/pages/homepage.dart
+++ lib/presentation/pages/homepage.dart
@@ -33,30 +33,37 @@ class MyHomePage extends StatelessWidget {
               ))
         ],
       ),
-      body: ListView(
-        children: const [
-          CategoryCard(
-            title: 'MATH',
-            imagePath: 'assets/icones/1.png',
-          ),
-          CategoryCard(
-            title: 'Physics',
-            imagePath: 'assets/icones/4.png',
-          ),
-          CategoryCard(
-            title: 'SVT',
-            imagePath: 'assets/icones/2.png',
-          ),
-          CategoryCard(
-            title: 'Others',
-            imagePath: 'assets/icones/3.png',
-          ),
-          CategoryCard(
-            title: 'Others',
-            imagePath: 'assets/icones/3.png',
-          ),
-        ],
-      ),
+      body: ListCategories(),
     ));
+  }
+}
+
+class ListCategories extends StatelessWidget {
+  const ListCategories({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        CategoryCard(
+          title: 'MATH',
+          imagePath: 'assets/icones/1.png',
+        ),
+        CategoryCard(
+          title: 'Physics',
+          imagePath: 'assets/icones/4.png',
+        ),
+        CategoryCard(
+          title: 'SVT',
+          imagePath: 'assets/icones/2.png',
+        ),
+        CategoryCard(
+          title: 'Others',
+          imagePath: 'assets/icones/3.png',
+        ),
+      ],
+    );
   }
 }
