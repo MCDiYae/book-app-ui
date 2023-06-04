d82edb1edaa4301d7eb399c64c95fd1ecdcefefd
MCDiYae <meddiyae.ghbalou@gmail.com>
2023-06-04 16:00:18 +0100

    module page complet UI

--- lib/presentation/pages/module/widgets/book_card.dart
+++ lib/presentation/pages/module/widgets/book_card.dart
@@ -2,19 +2,43 @@ import 'package:bookappui/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class BookCard extends StatelessWidget {
-  const BookCard({super.key});
+  final String moduleTitle;
+  final String moduleimagePath;
+  final String moduleDescription;
+  const BookCard(
+      {super.key,
+      required this.moduleTitle,
+      required this.moduleimagePath,
+      required this.moduleDescription});
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
-      child: Column(
-        children: [
-          Text(
-            'titre',
-            style: Styles.sTyle1,
-          ),
-          Row(),
-        ],
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Card(
+        child: Column(
+          children: [
+            Text(
+              moduleTitle,
+              style: Styles.sTyle2,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Image.asset(
+                    moduleimagePath,
+                  ),
+                  Text(
+                      textAlign: TextAlign.center,
+                      moduleDescription,
+                      style: Styles.styleWriting),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
