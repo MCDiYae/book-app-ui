31433ada8133295696cb5bd84bfaaaa639ad0a4f
MCDiYae <meddiyae.ghbalou@gmail.com>
2023-06-26 20:29:44 +0100

    last update

--- lib/presentation/BLoC/cubit/app_bar_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'app_bar_state.dart';
-
-class AppBarCubit extends Cubit<AppBarState> {
-  AppBarCubit() : super(AppBarInitial());
-}
